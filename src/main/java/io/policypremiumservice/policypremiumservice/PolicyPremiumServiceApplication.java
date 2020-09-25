84cd844ae1e8054c75dc27544cdd468c1aec0bbb
Amrita25 <amrita.majumdar25@gmail.com>
2020-09-25 22:11:43 +0530

    first commit

---
+++ src/main/java/io/policypremiumservice/policypremiumservice/PolicyPremiumServiceApplication.java
@@ -0,0 +1,13 @@
+package io.policypremiumservice.policypremiumservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyPremiumServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyPremiumServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/policypremiumservice/policypremiumservice/PolicyPremiumServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.policypremiumservice.policypremiumservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyPremiumServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
